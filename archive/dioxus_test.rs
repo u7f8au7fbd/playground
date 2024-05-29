89c24977fd7ad89c39e90d8b41e87c04a58e03c5
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-05-29 11:23:26 +0900

    add Dioxus

---
+++ archive/dioxus_test.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        link { rel: "stylesheet", href: "main.css" }
+        div { id: "links",
+            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
+            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
+            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                "💫 VSCode Extension"
+            }
+            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        }
+    }
+}
