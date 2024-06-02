5e5d0b449bccb86d7d7f1b0e82c56224586dc0ef
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-06-02 10:23:15 +0900

    First

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,18 @@ fn setup() {
 
 fn main() {
     setup();
+    get_rnking("rust", 0);
+}
+
+fn format_query(query: &str) -> String {
+    query.replace(' ', "")
+}
+
+fn get_rnking(query: &str, page: i32) {
+    let query = format_query(query);
+    let url = format!("https://www.google.com/search?q={}&start={}", query, page);
+}
+
+fn print_query(query: &str) {
+    println!("検索クエリ: {}", query);
 }
