917058db3910682a1e3b9709d1692e7ad04891ec
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-06-03 17:34:47 +0900

    add googling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 #[macro_use]
 mod macros;
 
@@ -11,6 +10,12 @@ fn setup() {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup();
-
     Ok(())
+}
+
+fn googling(query: &str,page: u32)->String {
+    let _ = query.replace(' ', "+");
+    let query = "rust lang";
+    let url = format!("https://www.google.com/search?q={}&start={}", query, page);
+    url
 }
