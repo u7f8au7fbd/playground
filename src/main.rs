66f47c97f7bafd5a3e9b97447f96ea0bdc3b8ea4
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-06-07 17:24:48 +0900

    set

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,35 @@ fn setup() {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup();
-    reqwest_dl("https://www.google.com/search?q=Rust&start=0","./db/test/test_rq.html").await?;
-    powershell_dl("https://www.google.com/search?q=Rust&start=0","./db/test/test_ps.html").await?;
+    reqwest_dl(
+        "https://www.google.com/search?q=Rust&start=0",
+        "./db/test/test_rq.html",
+    )
+    .await?;
+    powershell_dl(
+        "https://www.google.com/search?q=Rust&start=0",
+        "./db/test/test_ps.html",
+    )
+    .await?;
     Ok(())
 }
 
-async fn reqwest_dl(url:&str,path:&str)->Result<(), Box<dyn std::error::Error>>{
+async fn reqwest_dl(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let html_data = reqwest::get(url).await?.bytes().await?;
     std::fs::write(path, html_data).expect("保存失敗");
     Ok(())
 }
 
-
-async fn powershell_dl(url: &str, path: &str)->Result<(), Box<dyn std::error::Error>>{
-    let output = Command::new("powershell")
-        .arg("-Command")
-        .arg(format!("Invoke-WebRequest -Uri {} -OutFile {}", url, path))
-        .output()?;
-    if !output.status.success() {
-        let error_message = String::from_utf8_lossy(&output.stderr);
-        return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            error_message.trim(),
-        )));
-    }
+async fn powershell_dl(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    Command::new("powershell")
+        .args([
+            "Invoke-WebRequest",
+            "-Uri",
+            "https://example.com",
+            "-OutFile",
+            "./db/TEST/test_ps.html",
+        ])
+        .output()
+        .expect("コマンド実行中にエラーが発生しました");
     Ok(())
 }
