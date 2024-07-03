e061ace06e8a2e034610c1797ba990f56e771452
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-07-03 17:32:33 +0900

    fix

--- doc/memo.md
+++ doc/memo.md
@@ -2,6 +2,7 @@
 ## Rust
 - contains()を使うと特定の文に特定のWordが含まれ得ているかを調べることができる．
 - iter.sum()でベクター内部の総和を出すことができる
+- to_string_prettyでフォーマットした形で保存できる
 ## GoogleURL
 - &start=10 :ページ開始指定
 - &sclient=mobile-gws-wiz-hp :モバイル
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 #[macro_use]
 mod macros;
 
@@ -10,6 +12,26 @@ fn setup() {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup();
-    cmd!(green_bg);
+    let path = "./test.json";
+    read_and_print_json(path)?;
+    Ok(())
+}
+
+fn read_and_print_json(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let json_str = std::fs::read_to_string(path)?;
+    let json: Value = serde_json::from_str(&json_str)?;
+    if let Value::Array(arr) = json {
+        for obj in arr {
+            if let Value::String(main_word) = obj["main_word"].clone() {
+                if let Value::Array(sub_word) = obj["sub_word"].clone() {
+                    for word in sub_word {
+                        if let Value::String(word) = word {
+                            println!("{}+{}", main_word, word);
+                        }
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
