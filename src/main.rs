357b5a0528baec207e23845bad4112df9c6c092c
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-07-24 00:14:39 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -73,7 +73,8 @@ async fn process_chunk(chunk: &[Vec<String>]) {
                     // 各Stringを並列処理するタスクを生成
                     let item_clone = item.clone();
                     task::spawn(async move {
-                        println!("{}", item_clone);
+                        //println!("{}", item_clone);
+                        reqwest_status(item_clone).await.unwrap();
                     })
                 })
                 .collect::<Vec<_>>()
@@ -84,7 +85,9 @@ async fn process_chunk(chunk: &[Vec<String>]) {
     for task in tasks {
         task.await.unwrap();
     }
-    println!("チャンク処理完了");
+    print!("チャンク処理完了");
+    cmd!(green_line); //1チャンクの処理が終わるたびに緑の線を表示する
+    tokio::time::sleep(std::time::Duration::from_millis(1760)).await;
 }
 
 #[tokio::main]
@@ -105,5 +108,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //Jsonをファイルに書き込む
     std::fs::write("output.json", json_str)?;
 
+    Ok(())
+}
+
+async fn reqwest_status(url: String) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::builder().build()?;
+    let response = client.get(&url)
+        .header("Cookie", "NID=515=mlGvH_06_7rOahVHX4mfBgoAnHNDr6C9XXyZkmdo2TSczwJSNVkfMoEGboxtcOXyGUo5PWN8L10cPBn2bfQ3uF4xl6D8UTwz8iH3ppGwEaWuNKd9uwVo6MAUkSj9typIaoihflNSuzPa644wg1OufpcGCzzOfWbVzenEHEcYdFZpzeOqJCpOfDefYncaNrsX-l8QJWOTFlw11khb-sNxV1_WasYZJUJAxRuZnwDeNbmnONlWN9W28ZCimS80gcXiAhO5PXjHVxQ530AMNI5L26EZp7L8_TMnIkYLDFh2xzW9v_pKelnxyASOd_lcB2-2jJlKjpwBVgBs2Snr6a1ScO-nGKMAfpt2bWhdSjNvR66d6IVmVBOF23j3AtRg_YNNBBNQUnW6i7aTm_uaZdSCHyq6KSc_mCrk1w--3muMjI9rOnOPnwB_qF-XC-o8EhycXj_J8l5V-jRwSaN2ARNtN1dVRKNLOSJ-9KkY6eF2Grp6oJ_k6Vd3Y7qiCmenxs7_TY7_U0fzn4Vs2YYfp6QQY_POeIkspGNnoUUc0n_oK04dpIp4C3w-ot0lfqUzC1B50RVnUI1a")
+        //.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+        .send()
+        .await?;
+    let status = response.status();
+    if status == reqwest::StatusCode::TOO_MANY_REQUESTS {
+        cmd!(red_line);
+        cmd!(red_line);
+        cmd!(red_line);
+        cmd!(red_line);
+        std::process::exit(0);
+    }
+    println!("HTTPSステータス: {}", status);
     Ok(())
 }
